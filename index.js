5c040beab46c67e9254c7b252389cc272532a04e
Adewole Adebayo <wole1105@gmail.com>
2022-06-04 01:28:27 -0700

    commit tree cf2568d87b18d0c6d1693836f1dd86aaa50fbb33

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, name) {
+    return drivers.filter(el => el.toLowerCase() === name.toLowerCase())
+}
+
+beforeEach(function () {
+    drivers.length = 0;
+
+    drivers.push('Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby');
+  });
+
+function fuzzyMatch(drivers, beginName) {
+    return drivers.filter(el => el.startsWith(beginName))
+}
+
+function matchName(drivers, nameMatch) {
+    return drivers.filter(el => el.name === nameMatch)
+}
